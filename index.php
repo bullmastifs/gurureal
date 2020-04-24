a148717fbe10d6c9f2673f6127ee7bd44c2e1199
bullmastifs <57349190+bullmastifs@users.noreply.github.com>
2020-04-24 14:21:44 +0300

    Final push
    All files published

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Real Estate Guru - Продажа и аренда недвижимости в Латвии, получение вида на жительство в Латвии.</title>
+	<meta name="author" content="ENTER7">
+	<meta name="description" content="Продажа и аренда недвижимости в Латвии, получение вида на жительство в Латвии. Купить или снять жилье в Латвии с помощью нашей компании."/>
+	<meta name="keywords" content="недвижимость в латвии, купить недвижимость в латвии, продажа недвижимости в латвии, латвия вид на жительство недвижимость, покупка недвижимости в латвии, вид на жительство в латвии, купить жилье в латвии, снять в латвии жилье, жилье в латвии">
+	<meta name="theme-color" content="#ffffff">
+	<meta name="google-site-verification" content="9Nnh0a1qwdjCbyd747zT_LFjNN4T6CK0GJ1E1tXyZgw" />
+	<meta property="og:title" content="Real Estate Guru - Продажа и аренда недвижимости в Латвии, получение вида на жительство в Латвии." />
+	<meta property="og:type" content="image/jpg" />
+	<meta property="og:url" content="#" />
+	<meta property="og:image" content="og-image.jpg" />
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+	<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<!-- <script async src="https://www.googletagmanager.com/gtag/js?id=UA-72831960-1"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+		gtag('config', 'UA-72831960-1');
+	</script> -->
+</head>
+<body>
+	<main>
+		<div id="top-scroll"></div>
+	</main>
+
+		<!-- First Wrapper -->
+
+		<div class="hero-image">
+			<div class="hero-text">
+				<div class="item1 logotips"></div>
+				<div class="title">
+					<h1>The website is under contruction</h1>
+				</div>
+				<div class="subline">
+					<p>We are currently working on this page.</p>
+					<p>If you have some questions, please contact us!</p>
+				</div>
+				<div class="mainbutton">
+					<a href="+371 29790279">+37129790279</a>
+					<a href="mailto:guru@gurureal@@?££lv" onmouseover="this.href=this.href.replace('@@?££','.')">guru@gurureal.lv</a><br>
+				</div>
+			</div>
+		</div>
+</body>
+</html>
